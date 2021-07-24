bd59ef662eb071fb750d27c91b0012ab42dafc04
Alain Buisseret <me@alain.dev>
2021-07-24 11:09:04 +0200

    Changed the algo with vector (much faster !)

--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,55 @@
 /// Compute the Levenshtein distance between two strings.
 ///
-/// The generic parameter `MAX` is the size of the square matrix used in the 
+/// See the [Wikipedia article](https://en.wikipedia.org/wiki/Levenshtein_distance) and the 
 /// [Wagner–Fischer algorithm](https://en.wikipedia.org/wiki/Wagner%E2%80%93Fischer_algorithm).
-/// 
-/// See the [Wikipedia article](https://en.wikipedia.org/wiki/Levenshtein_distance).
+/// This actual algorithm was inspired by [wooorm](https://github.com/wooorm/levenshtein-rs).
 ///
 /// # Example
 /// ```
 /// use levenshtein::lev;
 /// let a = "Hello";
 /// let b = "World";
-/// assert_eq!(4, lev::<6>(a, b));
+/// assert_eq!(4, lev(a, b));
 /// ```
-///
-/// # Panics
-/// May panic if `a` or `b` has a length which is greater than `MAX - 1`.
-///
-pub fn lev<const MAX: usize>(a: &str, b: &str) -> usize {
-    let (a_len, b_len) = match (a.len(), b.len()) {
+pub fn lev(a: &str, b: &str) -> usize {
+    let b_len = match (a.len(), b.len()) {
         (0, b_len) => return b_len,
         (a_len, 0) => return a_len,
-        (a_len, b_len) => (a_len, b_len)
+        (_, b_len) => b_len
     };
     if a == b {
         return 0;
     }
-    let mut d: [[usize ; MAX] ; MAX] = [[0 ; MAX] ; MAX];
-    for i in 1..=a_len {
-        d[i][0] = i;
-    }
-    for j in 1..=b_len {
-        d[0][j] = j;
-    }
 
-    let mut b_iter = b.chars();
-    for j in 1..=b_len {
-        let b_char = b_iter.next();
-        let mut a_iter = a.chars();
-        for i in 1..=a_len {
-            let a_char = a_iter.next();
-            let cost = if a_char == b_char {
-                0
-            } else {
-                1
-            };
+    let mut current = 0_usize;
+    let mut left;
+    let mut up;
+    let mut left_up = 0_usize;
+    let mut row_up = (1..).take(b_len).collect::<Vec<usize>>();
 
-            d[i][j] = usize::min(
-                d[i-1][j] + 1,
+    for (a_idx, a_char) in a.chars().enumerate() {
+        left = a_idx + 1;
+        for (b_idx, b_char) in b.chars().enumerate() {
+            up = row_up[b_idx];
+            let cost = if a_char == b_char { 0 } else { 1 };
+            current = usize::min(
+                left + 1,
                 usize::min(
-                    d[i][j-1] + 1,
-                    d[i-1][j-1] + cost
+                    up + 1,
+                    left_up + cost
                 )
             );
+            row_up[b_idx] = current;
+            left = current;
+            left_up = up;
         }
     }
-    d[a_len][b_len]
+    current
 }
 
 #[cfg(test)]
 mod tests {
-    use super::lev as g_lev;
-
-    #[inline]
-    fn lev(a: &str, b: &str) -> usize {
-        g_lev::<100>(a, b)
-    }
+    use super::lev;
 
     #[test]
     fn empty_strings() {
